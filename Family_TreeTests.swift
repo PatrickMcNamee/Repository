7c0d60ae56ada70aa8ee462e6883325b2b265f20
Pat McNamee <pmcnamee710@gmail.com>
2017-01-27 17:43:44 -0600

    Add files via upload

---
+++ Family_TreeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Family_TreeTests.swift
+//  Family TreeTests
+//
+//  Created by Pat William McNamee on 2/19/15.
+//  Copyright (c) 2015 Pat William McNamee. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Family_TreeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Family Tree
+//
+//  Created by Pat William McNamee on 2/19/15.
+//  Copyright (c) 2015 Pat William McNamee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
